2fc8879941f3156bc36a25de6baf7c9ed50fe54f
vhaerter <haerter.valentin@gmail.com>
2023-05-29 16:10:05 +0200

    Updated main_atomic.c

--- main_atomic.c
+++ main_atomic.c
@@ -8,22 +8,14 @@
 
 const int MAX_CNT = 100 * 1000 * 1000;
 
-atomic_int balance = ATOMIC_VAR_INIT(0);
-sem_t global_sem;
-
-void *change_balance(void *args) {
-    int64_t *amt = (int64_t *) args;
-
-    sem_wait(&global_sem);
-    balance = balance + *amt;
-    int64_t new_balance = balance;
-    sem_post(&global_sem);
-
-    return (void *) new_balance;
-}
+atomic_llong balance = ATOMIC_VAR_INIT(2000);
 
 void *change_balance_atomic(void *args) {
+    int64_t *amt = (int64_t *) args;
 
+    int64_t new_balance = atomic_fetch_add(&balance, *amt) + *amt;
+
+    return (void *) new_balance;
 }
 
 int main(int argc, char ** argv) {
@@ -41,22 +33,21 @@ int main(int argc, char ** argv) {
     if (argc >= 4)
         repetitions = strtoul(argv[3], NULL, 10);
 
-    balance = original_balance;
+    atomic_store(&balance, original_balance);
     printf("Input: n_threads: %u, balance: %lld\n", n_threads, balance);
 
     pthread_array = calloc(n_threads, sizeof(pthread_t));
     random_values = calloc(n_threads, sizeof(int64_t));
     results = calloc(n_threads, sizeof(int64_t *));
-    sem_init(&global_sem, 0, 1);
 
     srand(time(NULL));
     for (int i = 0; i < repetitions; i += (int) n_threads) {
         for (size_t si = 0; si < n_threads; si++) {
-            random_sign = rand() % 1000;
-            random_sign = (random_sign > 500) ? 1 : -1;
-            random_values[si] = random_sign * (rand() % 500);
+            random_sign = rand() % 10;
+            random_sign = (random_sign > 5) ? 1 : -1;
+            random_values[si] = random_sign * (rand() % 100);
             difference += random_values[si];
-            pthread_create(&pthread_array[si], NULL, &change_balance, &random_values[si]);
+            pthread_create(&pthread_array[si], NULL, &change_balance_atomic, &random_values[si]);
         }
 
         for (size_t si = 0; si < n_threads; si++) {
@@ -64,14 +55,15 @@ int main(int argc, char ** argv) {
         }
     }
 
+    const long long final_balance = atomic_load(&balance);
+
     printf("Original balance: \t%+lld\n"
            "New balance: \t\t%+lld\n"
            "Measured difference: \t%+lld\n"
            "Actual difference: \t%+lld\n",
-           original_balance, balance, difference, balance - original_balance);
+           original_balance, final_balance, difference, final_balance - original_balance);
 
     free(pthread_array);
     free(random_values);
     free(results);
-    sem_destroy(&global_sem);
 }

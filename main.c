7b9d49f94d685e77d14f99578c07e5e32e7823ce
vhaerter <haerter.valentin@gmail.com>
2023-05-29 16:54:49 +0200

    Kommentieren

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdatomic.h>
 #include <inttypes.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -14,9 +13,11 @@ sem_t global_sem;
 void *change_balance(void *args) {
     int64_t *amt = (int64_t *) args;
 
+    // Blockieren des Threads, falls anderer Thread derzeit im kritischen Bereich
     sem_wait(&global_sem);
     balance = balance + *amt;
     int64_t new_balance = balance;
+    // Freigeben der Blockade nach Verlassen des kritischen Bereichs
     sem_post(&global_sem);
 
     return (void *) new_balance;
@@ -27,6 +28,8 @@ void *change_balance_atomic(void *args) {
 }
 
 int main(int argc, char ** argv) {
+    // Dokumentation siehe main_atomic.c
+
     unsigned int n_threads = 4, repetitions = MAX_CNT;
     pthread_t *pthread_array;
     int64_t *random_values, random_sign, original_balance = 2000, difference = 0;
@@ -34,19 +37,19 @@ int main(int argc, char ** argv) {
 
     if (argc >= 2)
         n_threads = strtoul(argv[1], NULL, 10);
-
     if (argc >= 3)
         original_balance = strtol(argv[2], NULL, 10);
-
     if (argc >= 4)
         repetitions = strtoul(argv[3], NULL, 10);
 
     balance = original_balance;
-    printf("Input: n_threads: %u, balance: %lld\n", n_threads, balance);
+    printf("Input: n_threads: %u, balance: %lld, repetitions: %u\n", n_threads, balance, repetitions);
 
     pthread_array = calloc(n_threads, sizeof(pthread_t));
     random_values = calloc(n_threads, sizeof(int64_t));
     results = calloc(n_threads, sizeof(int64_t *));
+
+    // Intialisieren der globalen Semaphore mit Wert 1
     sem_init(&global_sem, 0, 1);
 
     srand(time(NULL));
@@ -73,5 +76,7 @@ int main(int argc, char ** argv) {
     free(pthread_array);
     free(random_values);
     free(results);
+
+    // Zerstören der Semaphore
     sem_destroy(&global_sem);
 }
--- main_atomic.c
+++ main_atomic.c
@@ -3,39 +3,52 @@
 #include <stdatomic.h>
 #include <inttypes.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include <time.h>
 
 const int MAX_CNT = 100 * 1000 * 1000;
 
+// Defaultwert von balance liegt bei 2000
 atomic_llong balance = ATOMIC_VAR_INIT(2000);
 
 void *change_balance_atomic(void *args) {
     int64_t *amt = (int64_t *) args;
 
+    // Abspeichern des neuen Kontostandes in eigener Variable
+    // balance wird um amt verändert
     int64_t new_balance = atomic_fetch_add(&balance, *amt) + *amt;
 
     return (void *) new_balance;
 }
 
 int main(int argc, char ** argv) {
+    // Defaultwert der Anzahl Threads = 4, Defaultwert der insg. Berechnungen = MAX_CNT
     unsigned int n_threads = 4, repetitions = MAX_CNT;
-    pthread_t *pthread_array;
-    int64_t *random_values, random_sign, original_balance = 2000, difference = 0;
-    int64_t **results;
 
+    pthread_t *pthread_array; // Array der benötigten Threads
+
+    int64_t *random_values, random_sign, original_balance = 2000, difference = 0;
+    // random_values: Array für Übergabe zufälliger Werte an die jeweiligen Threads
+    // random_sign: Variable für Bestimmung eines zufälligen Vorzeichens
+    // original_balance: Defaultwert auf 2000
+    // difference: Variable zur Messung der tatsächlich gemessenen Veränderung von balance
+
+    int64_t **results; // Array für Ergebnisse der Threads bei pthread_join
+
+    // Keine Fehlerbehandlung bei falscher Eingabe. Es wird erwartet, dass der Benutzer die Kommandozeile
+    // korrekt verwendet. Bei Weglassen der Argumente werden die Defaultwerte verwendet.
+    // ./main_atomic [Anzahl Threads] [Initialwert Kontostand] [Anzahl Berechnungen]
     if (argc >= 2)
         n_threads = strtoul(argv[1], NULL, 10);
-
     if (argc >= 3)
         original_balance = strtol(argv[2], NULL, 10);
-
     if (argc >= 4)
         repetitions = strtoul(argv[3], NULL, 10);
 
+    // Wenn explizit angegeben, wird original_balance in balance hineingeschrieben
     atomic_store(&balance, original_balance);
-    printf("Input: n_threads: %u, balance: %lld\n", n_threads, balance);
+    printf("Input: n_threads: %u, balance: %lld, repetitions: %u\n", n_threads, balance, repetitions);
 
+    // Allozieren der Arrays
     pthread_array = calloc(n_threads, sizeof(pthread_t));
     random_values = calloc(n_threads, sizeof(int64_t));
     results = calloc(n_threads, sizeof(int64_t *));
@@ -43,10 +56,11 @@ int main(int argc, char ** argv) {
     srand(time(NULL));
     for (int i = 0; i < repetitions; i += (int) n_threads) {
         for (size_t si = 0; si < n_threads; si++) {
+            // Erzeugen eines zufälligen Wertes zwischen 0 und 100 mit zufälligem Vorzeichen
             random_sign = rand() % 10;
             random_sign = (random_sign > 5) ? 1 : -1;
             random_values[si] = random_sign * (rand() % 100);
-            difference += random_values[si];
+            difference += random_values[si]; // Messen der tatsächlichen Differenz
             pthread_create(&pthread_array[si], NULL, &change_balance_atomic, &random_values[si]);
         }
 
@@ -55,14 +69,16 @@ int main(int argc, char ** argv) {
         }
     }
 
+    // Abspeichern des Wertes von balance in lokaler Variable
     const long long final_balance = atomic_load(&balance);
 
-    printf("Original balance: \t%+lld\n"
-           "New balance: \t\t%+lld\n"
-           "Measured difference: \t%+lld\n"
-           "Actual difference: \t%+lld\n",
+    printf("Ursprünglicher Kontostand: \t%+lld\n"
+           "Neuer Kontostand: \t\t%+lld\n"
+           "Gemessene Differenz: \t%+lld\n"
+           "Tatsächliche Differenz: \t%+lld\n",
            original_balance, final_balance, difference, final_balance - original_balance);
 
+    // Befreien der Arrays
     free(pthread_array);
     free(random_values);
     free(results);

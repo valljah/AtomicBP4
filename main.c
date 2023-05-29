2f1db17d8dcbcb1cd82771acb0dce61ec92f1710
vhaerter <haerter.valentin@gmail.com>
2023-05-29 12:11:03 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int MAX_CNT = 100*1000*1000;
+
+double balance;
+sem_t global_sem;
+
+void *change_balance(void *args) {
+    double *amt = (double *) args;
+    double *new_balance = malloc(sizeof(double));
+
+    sem_wait(&global_sem);
+    balance = balance + *amt;
+    *new_balance = balance;
+    sem_post(&global_sem);
+
+    return (void *) new_balance;
+}
+
+int main(int argc, char ** argv) {
+    unsigned int n_threads = 4;
+    pthread_t *pthread_array;
+    double *random_values, random_sign, original_balance = 2000.0, difference = 0.0;
+    double **results;
+
+    if (argc >= 2)
+        n_threads = strtol(argv[1], NULL, 10);
+
+    if (argc == 3)
+        original_balance = strtod(argv[2], NULL);
+
+    pthread_array = calloc(n_threads, sizeof(pthread_t));
+    random_values = calloc(n_threads, sizeof(double));
+    results = calloc(n_threads, sizeof(double *));
+    sem_init(&global_sem, 0, 0);
+
+    srand(time(NULL));
+    for (int i = 0; i < MAX_CNT; i++) {
+        for (size_t si = 0; si < n_threads; si++) {
+            random_sign = rand() % 1000;
+            random_sign = (random_sign > 500) ? 1.0 : -1.0;
+            random_values[si] = random_sign * (rand() % 500);
+            difference += random_values[si];
+            pthread_create(&pthread_array[si], NULL, &change_balance, &random_values[si]);
+        }
+
+        for (size_t si = 0; si < n_threads; si++) {
+            pthread_join(pthread_array[si], (void**) &results[si]);
+            printf("New balance: %f\n", *results[si]);
+            free(results[si]);
+        }
+    }
+
+    free(pthread_array);
+    free(random_values);
+    free(results);
+    sem_destroy(&global_sem);
+}

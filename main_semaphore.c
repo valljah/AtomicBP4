ebd310195b31ae92c7b5d40011f8b8e7fdd71ea4
vhaerter <haerter.valentin@gmail.com>
2023-05-29 17:46:21 +0200

    Reworked main_atomic.c and added main_semaphore.c

--- main_semaphore.c
+++ main_semaphore.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdatomic.h>
+#include <semaphore.h>
 #include <inttypes.h>
 #include <pthread.h>
 #include <time.h>
@@ -8,8 +8,9 @@
 const int MAX_CNT = 100 * 1000 * 1000;
 int calculations_per_thread;
 
-// Globale Kontostandvariable balance
-atomic_llong balance;
+// Globale Semaphore
+sem_t global_sem;
+int64_t balance;
 
 void *change_balance_atomic(void *args) {
     int64_t random_sign, random_value;
@@ -26,7 +27,9 @@ void *change_balance_atomic(void *args) {
 
         // Messen der tatsächlichen Differenz
         *difference_per_thread += random_value;
-        atomic_fetch_add(&balance, random_value);
+        sem_wait(&global_sem);
+        balance += random_value;
+        sem_post(&global_sem);
     }
 
     return (void *) difference_per_thread;
@@ -56,7 +59,7 @@ int main(int argc, char **argv) {
         repetitions = strtoul(argv[3], NULL, 10);
 
     // Wenn explizit angegeben, wird original_balance in balance hineingeschrieben
-    atomic_store(&balance, original_balance);
+    balance = original_balance;
     // Jeder Thread führt jeweils repetitions / n_threads Rechenschritte durch
     calculations_per_thread = repetitions / n_threads;
     printf("Input: n_threads: %u, balance: %lld, repetitions: %u\n", n_threads, balance, repetitions);
@@ -69,6 +72,9 @@ int main(int argc, char **argv) {
     // Seed für den RNG basierend auf Systemzeit
     srand(time(NULL));
 
+    // Initialisieren der globalen Semaphore mit Wert 1
+    sem_init(&global_sem, 0, 1);
+
     for (size_t si = 0; si < n_threads; si++)
         pthread_create(&pthread_array[si], NULL, &change_balance_atomic, NULL);
 
@@ -79,17 +85,17 @@ int main(int argc, char **argv) {
         free(results[si]);
     }
 
-    // Abspeichern des Wertes von balance in lokaler Variable
-    const long long final_balance = atomic_load(&balance);
-
     printf("Ursprünglicher Kontostand: \t %lld\n"
            "Neuer Kontostand: \t\t %lld\n"
            "Gemessene Differenz: \t\t%+lld\n"
            "Tatsächliche Differenz: \t%+lld\n",
-           original_balance, final_balance, difference, final_balance - original_balance);
+           original_balance, balance, difference, balance - original_balance);
 
     // Befreien der Arrays
     free(pthread_array);
     free(random_values);
     free(results);
+
+    // Zerstören der globalen Semaphore
+    sem_destroy(&global_sem);
 }
